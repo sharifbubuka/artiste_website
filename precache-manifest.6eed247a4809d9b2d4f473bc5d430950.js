8b7de2f1200af25c336bab4e13d5f48ade81553e
Bubuka Sharif <sharifbubuka256@gmail.com>
2020-10-31 20:02:14 +0300

    Updates

---
+++ precache-manifest.6eed247a4809d9b2d4f473bc5d430950.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfbc9e919f2791f2ecfeb4bf94932b2c",
+    "url": "/artiste_website/index.html"
+  },
+  {
+    "revision": "0e11dd9694c5a12b4975",
+    "url": "/artiste_website/static/css/main.f1469958.chunk.css"
+  },
+  {
+    "revision": "20d730a411bd3d2c28e1",
+    "url": "/artiste_website/static/js/2.0fcfba1e.chunk.js"
+  },
+  {
+    "revision": "25ee36cecf3742d8e72fa8504c054097",
+    "url": "/artiste_website/static/js/2.0fcfba1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e11dd9694c5a12b4975",
+    "url": "/artiste_website/static/js/main.4230f475.chunk.js"
+  },
+  {
+    "revision": "efb7e2d9d6c754ee335f",
+    "url": "/artiste_website/static/js/runtime-main.85b24436.js"
+  },
+  {
+    "revision": "d90eeec1cf62097aab79873c11c153b8",
+    "url": "/artiste_website/static/media/applemusic.d90eeec1.svg"
+  },
+  {
+    "revision": "f07891bd51b539484d01463d11ac10d4",
+    "url": "/artiste_website/static/media/audiomack.f07891bd.svg"
+  },
+  {
+    "revision": "387f80ceeaf12b627d8446e012f9f162",
+    "url": "/artiste_website/static/media/logo.387f80ce.png"
+  },
+  {
+    "revision": "888e6582a430e50efc0d4f7b0c173c91",
+    "url": "/artiste_website/static/media/soundcloud.888e6582.svg"
+  },
+  {
+    "revision": "aee4e77e7cf867e46ec2c2fff268e9ad",
+    "url": "/artiste_website/static/media/spotify.aee4e77e.svg"
+  },
+  {
+    "revision": "d57a47e161639189da5621dd9cef0c1f",
+    "url": "/artiste_website/static/media/youtube.d57a47e1.svg"
+  }
+]);
